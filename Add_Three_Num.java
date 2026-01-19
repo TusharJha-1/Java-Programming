582cc94be7a3d51fdd4daee7728e30704104e27d
Tushar Jha <tusharjha.cse@gmail.com>
2026-01-19 12:56:55 +0530

    File Commited

---
+++ Add_Three_Num.java
@@ -0,0 +1,9 @@
+public class Add_Three_Num {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 15;
+        int c = 20;
+        int sum = a + b+ c;
+        System.out.println(sum);
+    }
+}
