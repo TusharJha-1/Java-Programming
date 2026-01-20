1a219b18e3d0fca7c84c79a995adb5cf91de7908
Tushar Jha <tusharjha.cse@gmail.com>
2026-01-21 00:18:41 +0530

    Deleted

--- A.java
+++
@@ -1,9 +0,0 @@
-// public class Abc {
-//     public static void main(String[] args) {
-//         StringBuilder sb = new StringBuilder("Hello");
-//         for(int i=sb.length()-1;i>=0;i--){
-//             System.out.print(sb.charAt(i));
-//         }
-//     }
-// }
-public class Abc{}
