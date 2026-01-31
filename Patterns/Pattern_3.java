951653220e68290a837b2bf3980575f0234d05ba
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-01 00:54:45 +0530

    Half Pyramid/FLoy'd Triangle/0-1 Triangle

--- Patterns/Pattern_3.java
+++ Patterns/Pattern_3.java
@@ -15,7 +15,6 @@ public class Pattern_3 {
 }
 
 //Flyd's Triangle
-
 // public class Pattern_3 {
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
@@ -26,6 +25,27 @@ public class Pattern_3 {
 //             for(int j=1;j<=i;++j){
 //                 System.out.print(cc+ " ");
 //                 ++cc;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// 0-1 Triangle
+
+// public class Pattern_3 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter height: ");
+//         int a = sc.nextInt();
+//         for(int i=1;i<=a;++i){
+//             for(int j=1;j<=i;++j){
+//                 if((i-j)%2==0){
+//                     System.out.print("1 ");
+//                 }
+//                 else{
+//                     System.out.print("0 ");
+//                 }
 //             }
 //             System.out.println();
 //         }
