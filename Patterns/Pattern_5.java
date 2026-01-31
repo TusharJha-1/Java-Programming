cb790d4fec18865d95a02464e22aceedcdffa4b5
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-01 00:31:20 +0530

    Half Pyramid (Right aligned)

---
+++ Patterns/Pattern_5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height : ");
+        int a = sc.nextInt();
+        for(int i=1;i<=a;++i){
+            for(int j=1;j<=a;++j){
+                if(i+j>a){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
