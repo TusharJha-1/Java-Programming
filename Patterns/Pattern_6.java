928df0df55991705e136d8d16eca31339f6bfb52
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-01 15:55:56 +0530

    Rhombus

---
+++ Patterns/Pattern_6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pattern_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of side : ");
+        int a = sc.nextInt();
+        for(int i=a;i>=1;--i){
+            for(int j=1;j<=i;++j){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=a;++k){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+for(int k=1;k<=a;++k){
+                System.out.print("* ");
+            }
