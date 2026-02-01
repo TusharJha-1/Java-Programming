7bc510a6eadc0115c14d51e0a292b7dfc8262487
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-01 19:55:39 +0530

    Diamond

---
+++ Patterns/Pattern_9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Pattern_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the order of pattern : ");
+        int a = sc.nextInt();
+        for(int i=1;i<=a;++i){
+            for(int j=1;j<=a-i;++j){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;++j){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=a;i>=1;--i){
+            for(int j=1;j<=a-i;++j){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;++j){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
