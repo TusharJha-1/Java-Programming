0ec8103b07f4ecc22bf8f78ea3ac1a40c38acad8
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-01 17:13:07 +0530

    Number Pyramid

--- Patterns/Pattern_7.java
+++ Patterns/Pattern_7.java
@@ -3,21 +3,17 @@ import java.util.Scanner;
 public class Pattern_7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a height number : ");
+        System.out.print("Enter length of side : ");
         int a = sc.nextInt();
-        for(int i=a;i>=1;--i){
-            for(int j=1;j<=i-1;++j){
-                System.out.print("+ ");
+        for(int i=1;i<=a;++i){
+            for(int j=1;j<=a-i;++j){
+                System.out.print(" ");
             }
-            for(int k=1;k<=a+(a-1);++k){
-                if(i-k==1 || i-k==-1 || i-k==3 || i-k==-3){
-                    System.out.print(i+" ");
-                }
-                else{
-                    System.out.print("  ");
-                }
+            for(int j=1;j<=i;++j){
+                System.out.print(i+" ");
             }
             System.out.println();
         }
     }
 }
+
