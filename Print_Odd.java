74994b28e3ed0782041bc1cf11d2a4e92356ae22
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-05 12:10:29 +0530

    Odd range print

--- Print_Odd.java
+++ Print_Odd.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class Print_Odd {
-    public static void printOdd(int n){
-        for(int i=1;i<=2*n;i+=2){
+    public static void printOdd(int m,int n){
+        int i = m;
+        while(i<=n){
             System.out.print(i+" ");
+            i+=2;
         }
         return;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of odd numbers you want to print : ");
+        System.out.print("Enter the start of range : ");
         int a = sc.nextInt();
-        printOdd(a);
+        System.out.print("Enter the end of range : ");
+        int b = sc.nextInt();
+        printOdd(a,b);
     }
 }
