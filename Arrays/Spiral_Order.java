59b2a4824759238413098c391675f3c3dc10bd33
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-12 12:23:23 +0530

    Spiral Order Traversal

--- Arrays/Spiral_Order.java
+++ Arrays/Spiral_Order.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Spiral_Order {
     public static void main(String[] args) {
-        int a=0,b=0,c=0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. of rows in the matrix : ");
         int rows = sc.nextInt();
