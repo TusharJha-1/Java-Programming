e9f88242305f58f8935c36693a001f21d40320ed
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-17 22:32:56 +0530

    Segregate the number of zeroes and ones

---
+++ Arrays/Segregate_0s_1s.java
@@ -0,0 +1,24 @@
+public class Segregate_0s_1s {
+    public static int[] segregate0and1(int []mat){
+        int n = mat.length;
+        int zero = 0;
+        int one = 0;
+        for(int i=0;i<n;++i){
+            if(mat[i]==0){++zero;}
+            else if(mat[i]==1){++one;}
+        }
+        for(int i=0;i<n;++i){
+            if(i<zero){mat[i]=0;}
+            else{mat[i]=1;}
+        }
+        return mat;
+    }
+    public static void main(String[] args) {
+        int []arr = {0,1,1,0,1,1,0,0,0};
+        arr = segregate0and1(arr);
+        for(int i=0;i<arr.length;++i){
+            System.out.print(arr[i]);
+        }
+
+    }
+}
