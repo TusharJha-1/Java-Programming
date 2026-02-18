91274a88f4305b3e2585d43e945f686539010650
Tushar Jha <tusharjha.cse@gmail.com>
2026-02-18 16:29:18 +0530

    Wave array form of sorted array

---
+++ Arrays/Wave_Array_Sorted.java
@@ -0,0 +1,17 @@
+public class Wave_Array_Sorted {
+    public static void wave(int []mat){
+        int n = mat.length;
+        for(int i=0;i<n-1;i+=2){
+            int t = mat[i];
+            mat[i] = mat[i+1];
+            mat[i+1] = t;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        wave(arr);
+        for(int i=0;i<arr.length;++i){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
