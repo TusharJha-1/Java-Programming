96281d7a9a3fdde3e842381e2523adf774252adf
Tushar Jha <tusharjha.cse@gmail.com>
2026-06-27 15:22:08 +0530

    Tower Of Hanoi

--- Recursion/Prog2.java
+++ Recursion/Prog2.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Prog2 {
     public static void hanoi(int n, String src,String help, String dest) {
         if(n==1){
-            System.out.println("Move disc from "+ src + " to "+ dest);
+            System.out.println("Move disc "+n+" from "+ src + " to "+ dest);
         }
         else{
             hanoi(n-1,src,dest,help);
-            System.out.println("Move disc from "+ src + " to "+ dest);
+            System.out.println("Move disc "+n+" from "+ src + " to "+ dest);
             hanoi(n-1,help,src,dest);
         }
     }
