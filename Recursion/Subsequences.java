b3f5fae7165d797ad20fb0937c05c52d8789aa4b
Tushar Jha <tusharjha.cse@gmail.com>
2026-06-28 02:28:41 +0530

    Subsequences

---
+++ Recursion/Subsequences.java
@@ -0,0 +1,17 @@
+public class Subsequences{
+    public static void subs(String s, int index, String ans){
+        if(index==s.length()){
+            System.out.println(ans);
+            return;
+        }
+        char currChar = s.charAt(index);
+        //to be 
+        subs(s,index+1,ans+currChar);
+        //not to be
+        subs(s,index+1,ans);
+    }
+    public static void main(String[] args) {
+        String s = "abc";
+        subs(s,0,"");
+    }
+}
