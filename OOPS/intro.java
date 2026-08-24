55c641686e61f1466cdc81c095e206d1504d544a
Tushar Jha <tusharjha.cse@gmail.com>
2026-08-24 23:52:28 +0530

    Update intro.java
    Classes and Objects, constructor( overloading and chaining), final(primitive and non-primitive reassignment case), garbage collection

--- OOPS/intro.java
+++ OOPS/intro.java
@@ -1,12 +1,91 @@
+import java.util.*;
 public class Intro {
     public static void main(String[] args) {
-        Student Tushar = new Student();
-        Tushar.rollNo[0] = 150;
-        System.out.println(Tushar.rollNo[1]);
+        Scanner sc = new Scanner(System.in);
+        Student[] students = new Student[1];
+        int n = students.length;
+
+        //Object creation for default constructor
+        Student stu = new Student();
+        //Two reference var(objects) pointing to same object
+        Student stu1 = stu;
+
+        //Object creation for single para constructor (Constructor Chaining)
+        Student stu2 = new Student(100);
+        System.out.println(stu1.name);
+
+        for(int i = 0;i < n; ++i){
+            System.out.println("Enter roll: ");
+            int rollNo = sc.nextInt();
+
+            System.out.println("Enter name: ");
+            String name = sc.next();
+
+            System.out.println("Enter marks: ");
+            float marks = sc.nextFloat();
+
+            //object creation for parameterized constructor
+            students[i] = new Student(rollNo, name, marks);
+        }
+        for (int i = 0; i < n; ++i) {
+            try{
+                students[i].display();
+            } catch(NullPointerException e){
+                System.out.println("{0,null,0.0}");
+            }
+        }
+        //stu.name = "Ram Jha";
+        stu.display();
+
+        final A obj1 = new A();
+        obj1.name = "Tushar Jha";
+        System.out.println(obj1.name);
+
+        //final keyword reassignment is not possible for non-primitive data types
+        //obj1 = new A("Ram Jha");
+
+        // final keyword reassignment/modification is not possible for primitive data types
+        try{
+            obj1.x = 20;
+        } catch(Exception e){
+            System.out.println("Final keyword reassignment/modification is not possible for primitive data types");
+        }
+        
+
     }
 }
+class A{
+    final int x = 10;
+    String name;
+    A(){
+        this.name = "Arjun Jha";
+    }
+}
+
 class Student{
-    int[] rollNo = new int[5];
-    String[] name = new String[5];
-    float[] marks = new float[5];
+    int rollNo;
+    String name = "Arjun Jha";
+    float marks;
+
+    //Function Overloading
+
+    Student(){
+        this.rollNo = 150;
+        //this.name = "Tushar Jha";
+        this.marks = 100;
+    }
+
+    Student(int roll){
+        this(101,"Contructor calling contructor",86.5f);
+    }
+
+    Student(int roll,String name,float marks){
+        this.rollNo = roll;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    void display(){
+        System.out.println("{"+rollNo+","+name+","+marks+"}");
+    }
 }
